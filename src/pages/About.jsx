1e582d42a8a1d491a00e1a54d8d35a8f9e355860
firuialloh <firuilalloh@gmail.com>
2025-10-13 23:56:10 +0700

    fix button

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,19 @@ function About() {
       <div className="min-h-screen bg-gray-900 text-white flex flex-col justify-center px-4">
         <div className="flex flex-col md:flex-row items-center md:items-start justify-center gap-10 w-full max-w-6xl mx-auto py-16">
           <div className="flex flex-col justify-center items-center md:items-start md:justify-start order-2 md:order-1 mt-15 text-center md:text-left">
-            <h1 className="text-5xl font-bold mb-3 tracking-tight">Mochammad Fafirru Ilalloh</h1>
-            <h2 className="text-2xl text-gray-400 font-light mb-2">Frontend Developer</h2>
-            <h2 className="text-2xl text-sky-400 mb-4 font-medium">1 Year & 9 Month Experience</h2>
+            <h1 className="text-5xl font-bold mb-3 tracking-tight">
+              Mochammad Fafirru Ilalloh
+            </h1>
+            <h2 className="text-2xl text-gray-400 font-light mb-2">
+              Frontend Developer
+            </h2>
+            <h2 className="text-2xl text-sky-400 mb-4 font-medium">
+              1 Year & 9 Month Experience
+            </h2>
             <p className="text-xl text-gray-300 mb-8 max-w-2xl md:text-justify">
               Passionate about creating beautiful, user-friendly web experiences
               with modern technologies. I love turning ideas into interactive
-              digital solutions. 
+              digital solutions.
             </p>
           </div>
           <div className="flex justify-center items-center order-1 md:order-2 md:mb-0 md:pl-20">
@@ -23,11 +29,13 @@ function About() {
             />
           </div>
         </div>
-        <div className="flex justify-center mt-2">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-full transition duration-200 shadow-lg">
-            My Work
-          </button>
-        </div>
+        <a href="#projects">
+          <div className="flex justify-center mt-2">
+            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-full transition duration-200 shadow-lg">
+              My Work
+            </button>
+          </div>
+        </a>
       </div>
     </section>
   );
